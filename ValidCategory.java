0097ca6097e744d9b44ba82781a1c48776a325be
Maayan-Shani <maayan200374@gmail.com>
2025-09-29 16:50:59 +0300

    Update ValidCategory.java

--- ValidCategory.java
+++ ValidCategory.java
@@ -1,7 +1,8 @@
-package OnlineStore.exceptions;
+package OnlineStore;
 
 public class ValidCategory extends Exception{
     public ValidCategory(String category){
         super("The category : " + category + " invalid. The categories are: Kids, Electricity, Office, Cloths. Please try again.");
     }
 }
+
